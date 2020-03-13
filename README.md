be7310e00cecb3c5bbd2a9c0bcf1b61c01124c30
SivaldoMuniz <sivaldo5@hotmail.com>
2020-03-12 23:15:04 -0300

    Criação Classe DAL/entidades

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CursosUdemy
+
+Curso de ASP.NET CORE + Entity Framework + DDD(Domain Drive Design).
---
+++ SistemaVenda/Entidades/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaVenda.Entidades
+    { public class Produto
+{
+    [Key]
+        public int? Codigo { get; set; }
+        public string  Descricao { get; set; }
+        public double Quantidade { get; set; }
+        public decimal Valor { get; set; }
+
+        [ForeignKey("Categoria")]
+        public int CodigoCategoria { get; set; }
+
+        public Categoria Categoria { get; set; }
+
+        public ICollection<VendaProdutos> Vendas { get; set; }
+
+      
+
+    }
+}
