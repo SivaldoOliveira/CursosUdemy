62bb709ac61697202a95f25743bbb89e2ac252cd
SivaldoMuniz <sivaldo5@hotmail.com>
2020-04-06 18:25:31 -0300

    Cadastro de Produtos

--- SistemaVenda/Controllers/ClienteController.cs
+++ SistemaVenda/Controllers/ClienteController.cs
@@ -8,17 +8,17 @@ using System.Linq;
 
 namespace SistemaVenda.Controllers
 {
-    public class CategoriaController : Controller
+    public class ClienteController : Controller
     {
         protected ApplicationDbContext mContext;
 
-        public CategoriaController(ApplicationDbContext context)
+        public ClienteController(ApplicationDbContext context)
         {
             mContext = context;
         }
         public IActionResult Index()
         {
-            IEnumerable<Categoria> lista = mContext.Categoria.ToList();
+            IEnumerable<Cliente> lista = mContext.Cliente.ToList();
             mContext.Dispose();
             return View(lista);
         }
@@ -26,36 +26,42 @@ namespace SistemaVenda.Controllers
         [HttpGet]
         public IActionResult Cadastro(int? id)
         {
-            CategoriaViewModel viewModel = new CategoriaViewModel();
+            ClienteViewModel viewModel = new ClienteViewModel();
 
             if (id != null)
             {
-                var entidade = mContext.Categoria.Where(x => x.Codigo == id).FirstOrDefault();
+                var entidade = mContext.Cliente.Where(x => x.Codigo == id).FirstOrDefault();
                 viewModel.Codigo = entidade.Codigo;
-                viewModel.Descricao = entidade.Descricao;
+                viewModel.Nome= entidade.Nome;
+                viewModel.CNPJ_CPF = entidade.CNPJ_CPF;
+                viewModel.Email = entidade.Email;
+                viewModel.Celular = entidade.Celular;
             }
 
             return View(viewModel);
         }
 
         [HttpPost]
-        public IActionResult Cadastro(CategoriaViewModel entidade)
+        public IActionResult Cadastro(ClienteViewModel entidade)
         {
             if (ModelState.IsValid)
             {
-                Categoria objCategoria = new Categoria()
+                Cliente objCliente = new Cliente()
                 {
                     Codigo = entidade.Codigo,
-                    Descricao = entidade.Descricao
+                    Nome = entidade.Nome,
+                    CNPJ_CPF = entidade.CNPJ_CPF,
+                    Email = entidade.Email,
+                    Celular =entidade.Celular
                 };
 
                 if (entidade.Codigo == null)
                 {
-                    mContext.Categoria.Add(objCategoria);
+                    mContext.Cliente.Add(objCliente);
                 }
                 else
                 {
-                    mContext.Entry(objCategoria).State = EntityState.Modified;
+                    mContext.Entry(objCliente).State = EntityState.Modified;
                 }
 
                 mContext.SaveChanges();
@@ -71,7 +77,7 @@ namespace SistemaVenda.Controllers
 
         public IActionResult Excluir(int id)
         {
-            var ent = new Categoria() { Codigo = id };
+            var ent = new Cliente() { Codigo = id };
             mContext.Attach(ent);
             mContext.Remove(ent);
             mContext.SaveChanges();
---
+++ SistemaVenda/Models/ProdutoViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace SistemaVenda.Models
+{
+    public class ProdutoViewModel
+    {
+        public int? Codigo { get; set; }
+
+        [Required(ErrorMessage="Informe a Descrição do Produto!")]
+        public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Informe a Quantidade em Estoque do Produto!")]
+        public double Quantidade{ get; set; }
+
+        [Required(ErrorMessage = "Informe o Valor unitário do Produto!")]
+        [Range(0.1, double.PositiveInfinity)]
+        public decimal? Valor { get; set; }
+
+        [Required(ErrorMessage = "Informe a Categoria do Produto!")]
+        public int? CodigoCategoria { get; set; }
+        public IEnumerable<SelectListItem> ListaCategorias { get; set; }
+
+    }
+}
